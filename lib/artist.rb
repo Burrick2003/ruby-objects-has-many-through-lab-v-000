61de5e6f394c1a8b1240a097ab79debee0455efd
Blakeley Bond <burrick2003@yahoo.com>
2017-11-19 02:51:33 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,12 @@
 class Artist
 attr_accessor :name, :songs
-  def initialize(name)
+  def initialize(name, genre)
     self.name = name
     self.songs = []
   end
 
-  def add_song(title, genre)
-    temp = Song.new(title, genre)
+  def add_song(title)
+    temp = Song.new(title)
     self.songs << temp
 
   end
