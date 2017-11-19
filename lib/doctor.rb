306e78e8763066bf353b665dfdf9306da74e8f5e
Blakeley Bond <burrick2003@yahoo.com>
2017-11-19 04:09:48 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,7 +1,7 @@
 class Doctor
-attr_accessor :doctor, :appointments
+attr_accessor :name, :appointments
   def initialize(name)
-    @doctor = name
+    @name = name
     @appointments = []
   end
   def add_appointment(appointment)
