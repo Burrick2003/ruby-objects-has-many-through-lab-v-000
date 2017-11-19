b95be32ebfb47901bcdeea80725c476b9f401240
Blakeley Bond <burrick2003@yahoo.com>
2017-11-19 03:36:32 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,7 @@ attr_accessor :appointments, :date, :doctor
   def initialize (date, doctor)
     @date = date
     #@doctor = doctor
-    doctor.add_appointment(self)
+    doctor.add_appointment(date)
   end
 
 
